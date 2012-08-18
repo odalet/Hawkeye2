d265fd2c344c0caf7ab3a1f60dc06405e1186092
odalet <odalet@gmail.com>
2012-08-19 00:59:15 +0200

    New API interface (IHawkeyeUI) + cleaned a bit the main form.

---
+++ src/Hawkeye.Api/UI/IHawkeyeUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hawkeye.UI
+{
+    /// <summary>
+    /// Gives access to the Hawkeye User interface to plugins
+    /// </summary>
+    public interface IHawkeyeUI
+    {
+        /// <summary>
+        /// Tells the main Hawkeye Form what Window to inspect.
+        /// </summary>
+        /// <param name="hwnd">The Window Handle.</param>
+        void SetTarget(IntPtr hwnd);
+    }
+}
