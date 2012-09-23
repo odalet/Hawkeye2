06f75fef4af3abe131072189d8c27740dd26f40b
odalet <odalet@gmail.com>
2012-09-23 14:26:32 +0200

    Fixed issue #2 (now Hawkeye test applications' assembly info is not tied to Hawkeye's one)

--- src/TestsAndTools/Hawkeye.DemoProject/Properties/AssemblyInfo.cs
+++ src/TestsAndTools/Hawkeye.DemoProject/Properties/AssemblyInfo.cs
@@ -1,6 +1,25 @@
-﻿using System.Reflection;
+﻿// Hawkeye is licensed under the terms of the Microsoft Reciprocal License (Ms-RL)
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
 #if DOTNET4
 [assembly: AssemblyTitle("Hawkeye.DemoProject.N4")]
 #else
 [assembly: AssemblyTitle("Hawkeye.DemoProject.N2")]
 #endif
+
+[assembly: AssemblyDescription("Hawkeye 2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://hawkeye.codeplex.com")]
+#if DOTNET4
+[assembly: AssemblyProduct("Hawkeye 2 Test Application - .NET 4")]
+#else
+[assembly: AssemblyProduct("Hawkeye 2 Test Application - .NET 2")]
+#endif
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("Hawkeye")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
