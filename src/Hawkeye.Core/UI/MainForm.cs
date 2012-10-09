50564f481a920ef736415e91ff4ec79b93b2cb9f
odalet <odalet@gmail.com>
2012-10-09 02:37:10 +0200

    Cleaned up dead code

--- src/Hawkeye.Core/UI/MainForm.cs
+++ src/Hawkeye.Core/UI/MainForm.cs
@@ -57,40 +57,8 @@ namespace Hawkeye.UI
             base.Text = string.Format("Hawkeye {0} - {1}",
                 HawkeyeApplication.CurrentClr,
                 HawkeyeApplication.CurrentBitness.ToString().ToLowerInvariant());
-        }
-
-        private void Inject(Clr clr, Bitness bitness)
-        {
-        }
-
-        private void injectN2x86Button_Click(object sender, EventArgs e)
-        {
-            Inject(Clr.Net2, Bitness.x86);
-        }
-
-        private void injectN2X64Button_Click(object sender, EventArgs e)
-        {
-            Inject(Clr.Net2, Bitness.x64);
-        }
-
-        private void injectN4x86Button_Click(object sender, EventArgs e)
-        {
-            Inject(Clr.Net4, Bitness.x86);
-        }
-
-        private void injectN4x64Button_Click(object sender, EventArgs e)
-        {
-            Inject(Clr.Net4, Bitness.x64);
-        }
-
-        private static int logCount = 0;
-        private void button1_Click(object sender, EventArgs e)
-        {
-            logCount++;
-            var l = Hawkeye.Logging.LogManager.GetLogger(GetType());
-            Hawkeye.Logging.LoggingExtensions.Verbose(l, "TEST LOGGING: " + logCount);
-        }
-
+        }        
+        
         private void SetWindowSettings(MainFormSettings settings)
         {
             //TODO: handle multiple-screens (and config changes!)
